de8485ab54c391bd4f7d6423ffb1c919ab6fcfbd
Navdeep Kante <65970997+smartnavdeep@users.noreply.github.com>
2020-06-22 12:39:57 +0530

    Update key_generate.py

--- key_generate.py
+++ key_generate.py
@@ -1,7 +1,14 @@
 from cryptography.fernet import Fernet
+from time import sleep
 
 key = Fernet.generate_key()
 print(key)
+
+sleep(1)
+
+# NEW FILE
 file = open("key.txt","wb")
 file.write(key)
 file.close()
+# Key written
+# End
